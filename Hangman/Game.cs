a6962cdfc50d66abc6212ef2fccc8d715b61a036
unknown <kylee.schreibs2@yahoo.com>
2019-06-26 21:09:59 -0500

    add WelcomeDescription method to Game class

--- Hangman/Game.cs
+++ Hangman/Game.cs
@@ -12,14 +12,26 @@ namespace Hangman
         Player playerOne;
         Player playerTwo;
         string [] ScoreToPart;
+        string guessedWord;
+
 
         // constructor
         public Game()
         {
             
-            ScoreToPart = new string [] {"nothing", "head", "left arm", "right arm", "left leg", "right leg" };
+            ScoreToPart = new string [] {"nothing", "head", "body", "left arm", "right arm", "left leg", "right leg" };
         }
 
         // member methods
+        public void WelcomeDescriptionPrompt()
+        {
+            Console.WriteLine("Welcome to Hangman!");
+            Console.ReadLine();
+            Console.WriteLine("Directions: \nThis game requires two players.\n"
+                + "Player one will input a word and player two will guess what letters are in that word.\n"
+                + "If player two's guess is correct, the letter will be appear in the slot(s) it belongs in.\n"
+                + "If the guess is incorrect, an appendage will be added to the hangman character.\n"
+                + "To win the game, player two must guess ALL the letter's in player one's selected word BEFORE the hangman every appendage.");
+        }
     }
 }
