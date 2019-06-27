e3d9b72da72a608cc3628d905faacccc90acd85c
unknown <kylee.schreibs2@yahoo.com>
2019-06-26 21:17:57 -0500

    add CreatePlayers and KickOff methods in Game class

--- Hangman/Game.cs
+++ Hangman/Game.cs
@@ -9,20 +9,25 @@ namespace Hangman
     public class Game
     {
         // member variables
-        Player playerOne;
-        Player playerTwo;
-        string [] ScoreToPart;
-        string guessedWord;
+        Player playerOne; // supplies the word 
+        Player playerTwo; // guesses the letters in playerOne's word
+        public string [] BodyParts;
+        public string guessedWord;
 
 
         // constructor
         public Game()
         {
             
-            ScoreToPart = new string [] {"nothing", "head", "body", "left arm", "right arm", "left leg", "right leg" };
+            BodyParts = new string [] {"nothing", "head", "body", "left arm", "right arm", "left leg", "right leg" };
         }
 
         // member methods
+        public void KickOff()
+        {
+            WelcomeDescriptionPrompt();
+            CreatePlayers();
+        }
         public void WelcomeDescriptionPrompt()
         {
             Console.WriteLine("Welcome to Hangman!");
@@ -32,6 +37,13 @@ namespace Hangman
                 + "If player two's guess is correct, the letter will be appear in the slot(s) it belongs in.\n"
                 + "If the guess is incorrect, an appendage will be added to the hangman character.\n"
                 + "To win the game, player two must guess ALL the letter's in player one's selected word BEFORE the hangman every appendage.");
+        }
+        public void CreatePlayers()
+        {
+            Console.WriteLine("Player one, please enter your name and then press enter when you're finished.");
+            playerOne.name = Console.ReadLine();
+            Console.WriteLine("Player two, please enter your name and then press enter when you're finished.");
+            playerTwo.name = Console.ReadLine();
         }
     }
 }
